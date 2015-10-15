b7a54b581cb58e2344753451b0e127bf19d8f85e
Keigh Rim <keigh.rim@gmail.com>
2015-10-15 16:22:39 -0400

    PAR: still working on PS parser

--- src/main/java/edu/brandeis/cs/lappsgrid/stanford/corenlp/Parser.java
+++ src/main/java/edu/brandeis/cs/lappsgrid/stanford/corenlp/Parser.java
@@ -3,119 +3,129 @@ package edu.brandeis.cs.lappsgrid.stanford.corenlp;
 import edu.brandeis.cs.lappsgrid.Version;
 import edu.brandeis.cs.lappsgrid.stanford.StanfordWebServiceException;
 import edu.brandeis.cs.lappsgrid.stanford.corenlp.api.IParser;
-import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreAnnotations.CharacterOffsetBeginAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.CharacterOffsetEndAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.ling.IndexedWord;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.XMLOutputter;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
-import edu.stanford.nlp.semgraph.SemanticGraphEdge;
-import edu.stanford.nlp.trees.Constituent;
-import edu.stanford.nlp.trees.GrammaticalRelation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
-import org.lappsgrid.discriminator.Discriminators;
+import edu.stanford.nlp.util.IntPair;
+import org.lappsgrid.serialization.Data;
+import org.lappsgrid.serialization.Serializer;
 import org.lappsgrid.serialization.json.JsonArr;
 import org.lappsgrid.serialization.json.JsonObj;
 import org.lappsgrid.serialization.json.LIFJsonSerialization;
+import org.lappsgrid.serialization.lif.Annotation;
+import org.lappsgrid.serialization.lif.Container;
+import org.lappsgrid.serialization.lif.View;
+import org.lappsgrid.vocabulary.Features;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.*;
 
-public class Parser extends AbstractStanfordCoreNLPWebService implements
-		IParser {
+import static org.lappsgrid.discriminator.Discriminators.Uri;
 
-	public Parser() {
+public class Parser extends AbstractStanfordCoreNLPWebService implements
+        IParser {
+    static final String PSVOCAB = "http://vocab.lappsgrid.org/PhraseStructure";
+
+    public Parser() {
         this.init(PROP_TOKENIZE, PROP_SENTENCE_SPLIT, PROP_PARSE);
-	}
+    }
 
     @Override
-    public String execute(LIFJsonSerialization json) throws StanfordWebServiceException {
-        String txt = json.getText();
-        JsonObj view  = json.newView();
-        json.newContains(view, "Parser", "parse:stanford", this.getClass().getName() + ":" + Version.getVersion());
-        // NLP processing
-        Annotation doc = new Annotation(txt);
-        snlp.annotate(doc);
+    public String execute(Container container) throws StanfordWebServiceException {
 
-        Map<String, String> map = new HashMap<String, String>();
-        int cntConst = 0;
-        List<CoreMap> list = doc.get(SentencesAnnotation.class);
-        int cntSent = 0;
-        for (CoreMap sent : list) {
-            JsonObj ann = json.newAnnotation(view);
-            int start = sent.get(CoreAnnotations.CharacterOffsetBeginAnnotation.class);
-            int end = sent.get(CoreAnnotations.CharacterOffsetEndAnnotation.class);
+        String text = container.getText();
 
-            json.setId(ann, "ps" + cntSent++);
-            json.setType(ann, "http://vocab.lappsgrid.org/PhraseStructure");
-            json.setStart(ann, start);
-            json.setEnd(ann, end);
+        // Prepare two containers, one for tokens and one for parse trees
+        View tokens = container.newView();
+        tokens.setId("v1");
+        tokens.addContains(Uri.TOKEN,
+                String.format("%s:%s", this.getClass().getName(), getVersion()),
+                "tokenizer:stanford");
+        View parses = container.newView();
+        parses.setId("v2");
+        parses.addContains(PSVOCAB,
+                String.format("%s:%s", this.getClass().getName(), getVersion()),
+                "syntacticparser:stanford");
+
+        edu.stanford.nlp.pipeline.Annotation annotation
+                = new edu.stanford.nlp.pipeline.Annotation(text);
+        snlp.annotate(annotation);
+        int sid = 0;
+        int cid = 0;
+        Map<String , String > map = new HashMap<>();
+        List<CoreMap> sents = annotation.get(SentencesAnnotation.class);
+
+        for (CoreMap sent : sents) {
+            // populate tokens container
+            int tid = 0;
+            for (CoreLabel token : sent.get(TokensAnnotation.class)) {
+                tokens.newAnnotation(String.format("tk_%d_%d", sid, tid), Uri.TOKEN,
+                        token.beginPosition(), token.endPosition());
+                tid++;
+            }
+
+            Annotation a = parses.newAnnotation(
+                    "ps" + (sid), PSVOCAB,
+                    sent.get(CharacterOffsetBeginAnnotation.class),
+                    sent.get(CharacterOffsetEndAnnotation.class));
             Tree root = sent.get(TreeAnnotation.class);
-            Queue<Tree> queue = new LinkedList<Tree>();
+            Queue<Tree> nodeQueue = new LinkedList<>();
+            IntPair pair = root.getSpan();
 
-            JsonArr constituents = new JsonArr();
-            json.setSentence(ann, sent.toString());
-            json.setFeature(ann, "penntree", root.pennString());
-            json.setFeature(ann, "constituents", constituents);
-            queue.add(root);
-            while(!queue.isEmpty()) {
-                Tree parent = queue.remove();
-                JsonObj constituent = new JsonObj();
-                constituents.put(constituent);
-                String key = parent.pennString();
-                String id = map.get(key);
-                if(id == null) {
-                    id = "cs" + cntConst++;
-                    map.put(key, id);
-                }
-                constituent.put("id", id);
-                constituent.put("@type", "http://vocab.lappsgrid.org/Constituent");
-                constituent.put("label", parent.label().value());
-                JsonObj feature = new JsonObj();
-                constituent.put("features", feature);
-                feature.put("penntree", parent.pennString());
-                List<Tree> childlist = parent.getChildrenAsList();
-                if (childlist.size() > 0) {
-                    JsonArr children = new JsonArr();
-                    constituent.put("children", children);
-                    for (Tree child : childlist) {
-                        String childkey = child.pennString();
-                        String childid = map.get(childkey);
-                        if(childid == null) {
-                            childid = "cs" + cntConst++;
-                            map.put(childkey, childid);
-                        }
-                        children.put(childid);
-                        queue.add(child);
-                    }
+            List<String> allConstituents = new LinkedList<>();
+
+//            a.addFeature(Features.Constituent.START, String.valueOf(pair.elems()[0]));
+//            a.addFeature(Features.Constituent.END, String.valueOf(pair.elems()[1]));
+            // TODO 150903 add pennString as well, somehow
+            // TODO continue from here to queue/deque children and put children names to parents' list of children
+
+            while (!nodeQueue.isEmpty()) {
+                Tree parent = nodeQueue.remove();
+            }
+
+
+            for (CoreLabel token : sent.get(TokensAnnotation.class)) {
+                String ner = token.ner();
+                if (ner != null && !ner.equalsIgnoreCase("O")) {
+                    Annotation a = view.newAnnotation(
+                            "ne" + (++sid), Uri.??? token.beginPosition(), token.endPosition());
+                    a.addFeature(Features.Token.WORD, token.value());
+                    a.addFeature(Features.Token.NER, ner);
                 }
             }
+            sid++;
+            a.addFeature(Features.PhraseStructure.CONSTITUENTS, allConstituents.toString());
         }
-        return json.toString();
+
+        // TODO 150903 LIF? JSONLD?
+//        Data<Container> data = new Data<>(Uri.LIF, container);
+        Data<Container> data = new Data<>(Uri.JSON_LD, container);
+        return Serializer.toJson(data);
     }
 
 
-	@Override
-	public String parse(String docs) {
-		Annotation annotation = new Annotation(docs);
-		snlp.annotate(annotation);
+    @Override
+    public String parse(String docs) {
+        edu.stanford.nlp.pipeline.Annotation annotation
+                = new edu.stanford.nlp.pipeline.Annotation(docs);
+        snlp.annotate(annotation);
 
-		StringWriter sw = new StringWriter();
-		PrintWriter writer = new PrintWriter(sw);  
-		List<CoreMap> sentences = annotation.get(SentencesAnnotation.class);
-		for (CoreMap sentence1 : sentences) {
-			for (Tree tree : sentence1.get(TreeAnnotation.class)) {				
-				tree.printLocalTree(writer);
-			}
-		}
-		// return null;
-		return sw.toString();
-	}
+        StringWriter sw = new StringWriter();
+        PrintWriter writer = new PrintWriter(sw);
+        List<CoreMap> sentences = annotation.get(SentencesAnnotation.class);
+        for (CoreMap sentence1 : sentences) {
+            for (Tree tree : sentence1.get(TreeAnnotation.class)) {
+                tree.printLocalTree(writer);
+            }
+        }
+        // return null;
+        return sw.toString();
+    }
 
 }
