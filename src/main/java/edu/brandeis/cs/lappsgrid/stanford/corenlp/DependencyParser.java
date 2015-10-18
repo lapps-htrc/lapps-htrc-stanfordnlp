5910d8b9abe1e9f64c733c538b67d3d6cdbc2bef
chunqishi <shicq@cs.brandeis.edu>
2015-10-19 00:19:56 +0800

    added dependency parser

--- src/main/java/edu/brandeis/cs/lappsgrid/stanford/corenlp/DependencyParser.java
+++ src/main/java/edu/brandeis/cs/lappsgrid/stanford/corenlp/DependencyParser.java
@@ -16,10 +16,12 @@ import edu.stanford.nlp.trees.GrammaticalRelation;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
+import groovy.json.JsonBuilder;
 import org.lappsgrid.discriminator.Discriminators;
-import org.lappsgrid.serialization.json.JsonArr;
-import org.lappsgrid.serialization.json.JsonObj;
-import org.lappsgrid.serialization.json.LIFJsonSerialization;
+import org.lappsgrid.serialization.Data;
+import org.lappsgrid.serialization.Serializer;
+import org.lappsgrid.serialization.lif.Container;
+import org.lappsgrid.serialization.lif.View;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -33,62 +35,115 @@ public class DependencyParser extends AbstractStanfordCoreNLPWebService implemen
 	}
 
     @Override
-    public String execute(LIFJsonSerialization json) throws StanfordWebServiceException {
-        String txt = json.getText();
-        JsonObj view  = json.newView();
-        json.newContains(view, "http://vocab.lappsgrid.org/DependencyStructure", "dependencyparser:stanford", this.getClass().getName() + ":" + Version.getVersion());
-        json.newContains(view, "http://vocab.lappsgrid.org/Token", "token:stanford", this.getClass().getName() + ":" + Version.getVersion());
-        // NLP processing
-        Annotation doc = new Annotation(txt);
+    public String execute(Container container) throws StanfordWebServiceException {
+        String text = container.getText();
+        View view = container.newView();
+        view.addContains("http://vocab.lappsgrid.org/DependencyStructure", this.getClass().getName(), Version.getVersion());
+        view.addContains(Discriminators.Uri.TOKEN , this.getClass().getName(), Version.getVersion());
+
+//        // NLP processing
+        Annotation doc = new Annotation(text);
         snlp.annotate(doc);
         Map<String, String> map = new HashMap<String, String>();
-
+//
         List<CoreMap> list = doc.get(SentencesAnnotation.class);
         int cntSent = 0;
         for (CoreMap sent : list) {
-            JsonObj ann = json.newAnnotation(view);
             int start = sent.get(CoreAnnotations.CharacterOffsetBeginAnnotation.class);
             int end = sent.get(CoreAnnotations.CharacterOffsetEndAnnotation.class);
-            json.setId(ann, "dp" + cntSent);
-            json.setType(ann, "http://vocab.lappsgrid.org/DependencyStructure");
-            json.setStart(ann, start);
-            json.setEnd(ann, end);
+            org.lappsgrid.serialization.lif.Annotation ann = view.newAnnotation("dp" + cntSent,
+                    "http://vocab.lappsgrid.org/DependencyStructure", start, end);
 
-
-            JsonArr dependencies = new JsonArr();
-            json.setSentence(ann, sent.toString());
-            json.setFeature(ann, "dependencies", dependencies);
-
+            ann.addFeature("sentence", sent.toString());
             SemanticGraph graph = sent.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
-
+//
             int cntEdge = 0;
+            List<Map> dependencies = new ArrayList<>();
             for(SemanticGraphEdge edge:graph.getEdgeSet()) {
-                JsonObj dep = new JsonObj();
-                dependencies.put(dep);
+                Map dep = new LinkedHashMap();
                 dep.put("id", "dep_"+cntSent+"_"+cntEdge++);
                 dep.put("label", edge.getRelation().toString());
-                JsonObj feats = new JsonObj();
+                Map feats = new LinkedHashMap();
                 dep.put("features", feats);
                 feats.put("governor", "tk" + cntSent + "_" + edge.getGovernor().index());
                 feats.put("governor_word", edge.getGovernor().word());
                 feats.put("dependent","tk" + cntSent + "_" + edge.getDependent().index());
                 feats.put("dependent_word", edge.getDependent().word());
+                dependencies.add(dep);
             }
-
+            ann.addFeature("dependencies", new JsonBuilder(dependencies).toPrettyString());
+            
             int cntToken = 1;
             for (CoreLabel token : sent.get(CoreAnnotations.TokensAnnotation.class)) {
-                ann = json.newAnnotation(view);
-                json.setId(ann, "tk" + cntSent + "_" + cntToken++);
-                json.setType(ann, Discriminators.Uri.TOKEN);
-                json.setStart(ann, token.beginPosition());
-                json.setEnd(ann, token.endPosition());
-                json.setWord(ann, token.value());
-                json.setFeature(ann, "pos", token.tag());
+                 ann = view.newAnnotation("tk" + cntSent + "_" + cntToken++,
+                         Discriminators.Uri.TOKEN, token.beginPosition(), token.endPosition());
+                ann.addFeature("pos", token.tag());
+                ann.addFeature("word", token.value());
             }
             cntSent ++;
         }
-        return json.toString();
+
+            Data<Container> data = new Data<>(Discriminators.Uri.LIF, container);
+            return Serializer.toJson(data);
     }
+//
+//    @Override
+//    public String execute(LIFJsonSerialization json) throws StanfordWebServiceException {
+//        String txt = json.getText();
+//        JsonObj view  = json.newView();
+//        json.newContains(view, "http://vocab.lappsgrid.org/DependencyStructure", "dependencyparser:stanford", this.getClass().getName() + ":" + Version.getVersion());
+//        json.newContains(view, "http://vocab.lappsgrid.org/Token", "token:stanford", this.getClass().getName() + ":" + Version.getVersion());
+//        // NLP processing
+//        Annotation doc = new Annotation(txt);
+//        snlp.annotate(doc);
+//        Map<String, String> map = new HashMap<String, String>();
+//
+//        List<CoreMap> list = doc.get(SentencesAnnotation.class);
+//        int cntSent = 0;
+//        for (CoreMap sent : list) {
+//            JsonObj ann = json.newAnnotation(view);
+//            int start = sent.get(CoreAnnotations.CharacterOffsetBeginAnnotation.class);
+//            int end = sent.get(CoreAnnotations.CharacterOffsetEndAnnotation.class);
+//            json.setId(ann, "dp" + cntSent);
+//            json.setType(ann, "http://vocab.lappsgrid.org/DependencyStructure");
+//            json.setStart(ann, start);
+//            json.setEnd(ann, end);
+//
+//
+//            JsonArr dependencies = new JsonArr();
+//            json.setSentence(ann, sent.toString());
+//            json.setFeature(ann, "dependencies", dependencies);
+//
+//            SemanticGraph graph = sent.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
+//
+//            int cntEdge = 0;
+//            for(SemanticGraphEdge edge:graph.getEdgeSet()) {
+//                JsonObj dep = new JsonObj();
+//                dependencies.put(dep);
+//                dep.put("id", "dep_"+cntSent+"_"+cntEdge++);
+//                dep.put("label", edge.getRelation().toString());
+//                JsonObj feats = new JsonObj();
+//                dep.put("features", feats);
+//                feats.put("governor", "tk" + cntSent + "_" + edge.getGovernor().index());
+//                feats.put("governor_word", edge.getGovernor().word());
+//                feats.put("dependent","tk" + cntSent + "_" + edge.getDependent().index());
+//                feats.put("dependent_word", edge.getDependent().word());
+//            }
+//
+//            int cntToken = 1;
+//            for (CoreLabel token : sent.get(CoreAnnotations.TokensAnnotation.class)) {
+//                ann = json.newAnnotation(view);
+//                json.setId(ann, "tk" + cntSent + "_" + cntToken++);
+//                json.setType(ann, Discriminators.Uri.TOKEN);
+//                json.setStart(ann, token.beginPosition());
+//                json.setEnd(ann, token.endPosition());
+//                json.setWord(ann, token.value());
+//                json.setFeature(ann, "pos", token.tag());
+//            }
+//            cntSent ++;
+//        }
+//        return json.toString();
+//    }
 
 
 	@Override
--- src/test/java/edu/brandeis/cs/lappsgrid/stanford/corenlp/TestDependencyParser.java
+++ src/test/java/edu/brandeis/cs/lappsgrid/stanford/corenlp/TestDependencyParser.java
@@ -2,6 +2,13 @@ package edu.brandeis.cs.lappsgrid.stanford.corenlp;
 
 import edu.brandeis.cs.lappsgrid.stanford.StanfordWebServiceException;
 import org.junit.Test;
+import org.lappsgrid.discriminator.Discriminators;
+import org.lappsgrid.metadata.ServiceMetadata;
+import org.lappsgrid.serialization.Data;
+import org.lappsgrid.serialization.Serializer;
+import org.lappsgrid.serialization.lif.Container;
+
+import java.util.Map;
 
 /**
  * <i>TestParser.java</i> Language Application Grids (<b>LAPPS</b>)
@@ -29,7 +36,15 @@ public class TestDependencyParser extends TestService {
 
     @Test
     public void testExecute(){
-        String ret = parser.execute("Hi, Programcreek is a very huge and useful website.");
+
+		String text = "Hi, Programcreek is a very huge and useful website.";
+		Data data = Serializer.parse(parser.getMetadata(), Data.class);
+		ServiceMetadata metadata = new ServiceMetadata((Map) data.getPayload());
+		Container container = new Container();
+		container.setText(text);
+		container.setLanguage("en");
+		container.setMetadata((Map) data.getPayload());
+		String ret = parser.execute(new Data<>(Discriminators.Uri.LIF, container).asPrettyJson());
 		System.out.println(ret);
 		System.out.println();
 //        Assert.assertTrue(ret.getPayload().contains("NN"));
